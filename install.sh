e6754f802b9cbbed8acfbb2dadaa5077683fe00a
Jack Ramey <jack.ramey@gmail.com>
2013-06-29 00:45:01 -0700

    Verbosity

--- install.sh
+++ install.sh
@@ -4,12 +4,16 @@ git rebase master
 git stash pop
 
 #backup old vimrc
+echo "Creating backup of .vimrc"
 cp $HOME/.vimrc $HOME/.vimrc.bak_$(date +"%Y_%m_%d")
 #copy new vimrc over
+echo "Copying new .vimrc to $HOME/"
 cp vim/vimrc $HOME/.vimrc
 
 #backup old zshrc
+echo "Creating backup of .zshrc"
 cp $HOME/.zshrc $HOME/.zshrc.back_$(date +"%Y_%m_%d")
 #copy new zshrc over
+echo "Copying new .zshrc to $HOME/"
 cp zsh/zshrc $HOME/.zshrc
 
