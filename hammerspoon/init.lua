e6a62125d573966d06f42e1bfbcd0fd61166fe9b
jack.ramey <jack.ramey@workday.com>
2020-05-29 10:42:08 -0700

    Updating config files for new setup
    Adding hammerspoon configuration. This replaces slate for window controls
    Adding karabiner configuration. This allows for simple and complex remapping of keys
    Adding iterm2 profiles for consistency across machines
    Updating zsh structure for flexibility. This is pretty trivial for personal setup

---
+++ hammerspoon/init.lua
@@ -0,0 +1,277 @@
+-- Remove animations
+hs.window.animationDuration=0
+
+HyperKey = {"cmd", "alt", "ctrl"}
+VyperKey = {"cmd", "alt", "ctrl", "shift"}
+
+---------------------
+-- Resize Bindings --
+---------------------
+
+function Middle(wPercent, hPercent)
+    return function()
+        local win = hs.window.focusedWindow()
+        local frame = win:screen():frame()
+        if wPercent < 1 then
+            local width = frame.w * wPercent
+            local wgap = frame.w - width
+            frame.w = width
+            frame.x = frame.x + (wgap / 2)
+        end
+        if hPercent < 1 then
+            local height = frame.h * hPercent
+            local hgap = frame.h - height
+            frame.h = height
+            frame.y = frame.y + (hgap / 2)
+        end
+        win:setFrame(frame)
+    end
+end
+
+function Full()
+    return Middle(1,1)
+end
+
+function Center(percent)
+    return Middle(percent, 1)
+end
+
+function Top(percent)
+    return function()
+        local win = hs.window.focusedWindow()
+        local frame = win:screen():frame()
+        if percent < 1 then
+            local height = frame.h * percent
+            frame.h = height
+        end
+        win:setFrame(frame)
+    end
+end
+
+function Bottom(percent)
+    return function()
+        local win = hs.window.focusedWindow()
+        local frame = win:screen():frame()
+        if percent < 1 then
+            local height = frame.h * percent
+            local hgap = frame.h - height
+            frame.h = height
+            frame.y = frame.y + hgap
+        end
+        win:setFrame(frame)
+    end
+end
+
+function Left(percent)
+    return LeftTop(percent, 1)
+end
+
+function LeftTop(wPercent, hPercent)
+    return function()
+        local win = hs.window.focusedWindow()
+        local frame = win:screen():frame()
+        frame.w = frame.w * wPercent
+        if hPercent < 1 then
+            frame.h = frame.h * hPercent
+        end
+        win:setFrame(frame)
+    end
+end
+
+function LeftBot(wPercent, hPercent)
+    return function()
+        local win = hs.window.focusedWindow()
+        local frame = win:screen():frame()
+        frame.w = frame.w * wPercent
+        if hPercent < 1 then
+            local height = frame.h * hPercent
+            local hgap = frame.h * (1.0 - hPercent)
+            frame.h = height
+            frame.y = frame.y + hgap
+        end
+        win:setFrame(frame)
+    end
+end
+
+function Right(percent)
+    return RightTop(percent, 1)
+end
+
+function RightTop(wPercent, hPercent)
+    return function()
+        local win = hs.window.focusedWindow()
+        local frame = win:screen():frame()
+        frame.x = frame.x + (frame.w - frame.w * wPercent)
+        frame.w = frame.w * wPercent
+        if hPercent < 1 then
+            frame.h = frame.h * hPercent
+        end
+        win:setFrame(frame)
+    end
+end
+
+function RightBot(wPercent, hPercent)
+    return function()
+        local win = hs.window.focusedWindow()
+        local frame = win:screen():frame()
+        frame.x = frame.x + (frame.w - frame.w * wPercent)
+        frame.w = frame.w * wPercent
+        if hPercent < 1 then
+            local height = frame.h * hPercent
+            local hgap = frame.h * (1.0 - hPercent)
+            frame.h = height
+            frame.y = frame.y + hgap
+        end
+        win:setFrame(frame)
+    end
+end
+
+function ExtendLeft(pixels)
+    return function()
+        local win = hs.window.focusedWindow()
+        local frame = win:frame()
+        frame.x = frame.x - pixels
+        frame.w = frame.w + pixels
+        win:setFrame(frame)
+    end
+end
+
+function ShrinkLeft(pixels)
+    return function()
+        local win = hs.window.focusedWindow()
+        local frame = win:frame()
+        frame.x = frame.x + pixels
+        frame.w = frame.w - pixels
+        win:setFrame(frame)
+    end
+end
+
+function ExtendRight(pixels)
+    return function()
+        local win = hs.window.focusedWindow()
+        local frame = win:frame()
+        frame.w = frame.w + pixels
+        win:setFrame(frame)
+    end
+end
+
+function ShrinkRight(pixels)
+    return function()
+        local win = hs.window.focusedWindow()
+        local frame = win:frame()
+        frame.w = frame.w - pixels
+        win:setFrame(frame)
+    end
+end
+
+function NextScreen()
+    return function()
+        local win = hs.window.focusedWindow()
+        local winFrame = win:frame()
+        local screen = win:screen()
+        local frame = screen:frame()
+        local nextScreen = screen:next()
+        local nextFrame = nextScreen:frame()
+        local dx = winFrame.x - frame.x
+        local dy = winFrame.y - frame.y
+        winFrame.x = nextFrame.x + dx
+        winFrame.y = nextFrame.y + dy
+        win:setFrame(winFrame)
+    end
+end
+
+function printFrame(frameName, frame)
+    hs.printf(frameName .. ": " .. frame.w .. " " .. frame.h)
+    hs.printf(frameName .. ": " .. frame.x .. " " .. frame.y)
+end
+
+
+--------------------
+-- Focus Bindings --
+--------------------
+
+switcher = hs.window.switcher.new(hs.window.filter.new():setCurrentSpace(true):setDefaultFilter{})
+switcher.ui.showTitles = true
+switcher.ui.showThumbnails = false
+switcher.ui.fontName = 'Hack'
+switcher.ui.backgroundColor = {0.0,0.0,0.0,0}
+switcher.ui.showSelectedTitle = false
+
+
+hs.hotkey.bind(HyperKey,'tab','',function()
+    switcher:next()
+end)
+
+--------------------
+-- Media Bindings --
+--------------------
+
+-- function changeVolume(diff)
+--     return function()
+--         local current = hs.audiodevice.defaultOutputDevice():volume()
+--         local new = math.min(100, math.max(0, math.floor(current + diff)))
+--         if new > 0 then
+--             hs.audiodevice.defaultOutputDevice():setMuted(false)
+--         end
+--         hs.audiodevice.defaultOutputDevice():setVolume(new)
+--     end
+-- end
+
+-- hs.hotkey.bind(HyperKey, 'down', changeVolume(-5))
+-- hs.hotkey.bind(HyperKey, 'up', changeVolume(5))
+
+-----------------------
+-- Location Bindings --
+-----------------------
+
+-- Switch to next screen
+hs.hotkey.bind(HyperKey, "R", NextScreen())
+
+-- Center
+hs.hotkey.bind(HyperKey, "space", Full())
+hs.hotkey.bind(VyperKey, "space", Center(.6))
+hs.hotkey.bind(VyperKey, "C", Center(.4))
+hs.hotkey.bind(HyperKey, "U", Top(.5))
+hs.hotkey.bind(HyperKey, "I", Bottom(.5))
+
+-- Left
+hs.hotkey.bind(VyperKey, "H", Left(.3))
+hs.hotkey.bind(HyperKey, "H", Left(.5))
+hs.hotkey.bind(HyperKey, "J", Left(.6))
+hs.hotkey.bind(VyperKey, "J", Left(.7))
+
+-- Right
+hs.hotkey.bind(VyperKey, "K", Right(.7))
+hs.hotkey.bind(HyperKey, "K", Right(.6))
+hs.hotkey.bind(HyperKey, "L", Right(.5))
+hs.hotkey.bind(VyperKey, "L", Right(.3))
+
+-- Left Quadrants
+hs.hotkey.bind(VyperKey, "O", LeftTop(.3, .5))
+hs.hotkey.bind(HyperKey, "O", LeftTop(.5, .5))
+hs.hotkey.bind(HyperKey, "N", LeftBot(.5, .5))
+hs.hotkey.bind(VyperKey, "N", LeftBot(.3, .5))
+
+-- Right Quadrants
+hs.hotkey.bind(VyperKey, "P", RightTop(.3, .5))
+hs.hotkey.bind(HyperKey, "P", RightTop(.5, .5))
+hs.hotkey.bind(HyperKey, "M", RightBot(.5, .5))
+hs.hotkey.bind(VyperKey, "M", RightBot(.3, .5))
+hs.hotkey.bind(HyperKey, "0", RightBot(.7, .7))
+
+-- Resize Window --
+
+-- hs.hotkey.bind(HyperKey, "left", ExtendLeft(5), nil, ExtendLeft(5))
+-- hs.hotkey.bind(VyperKey, "left", ShrinkRight(5), nil, ShrinkRight(5))
+-- hs.hotkey.bind(HyperKey, "right", ExtendRight(5), nil, ExtendRight(5))
+-- hs.hotkey.bind(VyperKey, "right", ShrinkLeft(5), nil, ShrinkLeft(5))
+
+---------------
+-- Shortcuts --
+---------------
+
+-- Lock Screen
+hs.hotkey.bind(HyperKey, 'escape', function() hs.caffeinate.startScreensaver() end)
+
+-- Reload Config
+hs.hotkey.bind(HyperKey, "f5", function() hs.reload() end)
---
+++ zsh/common.zsh
@@ -0,0 +1,79 @@
+alias refreshaudio='sudo pkill coreaudiod'# Path to your oh-my-zsh configuration.
+ZSH=$HOME/.oh-my-zsh
+
+# Set name of the theme to load.
+# Look in ~/.oh-my-zsh/themes/
+# Optionally, if you set this to "random", it'll load a random theme each
+# time that oh-my-zsh is loaded.
+ZSH_THEME="jagnoster"
+#noop
+
+#Sudo alias hack
+alias sudo='sudo '
+# Aliases
+alias zshconfig="vim ~/.zshrc"
+alias zshreload="source ~/.zshrc"
+alias sshconfig="vim ~/.ssh/config"
+# alias ohmyzsh="mate ~/.oh-my-zsh"
+alias ssh='ssh -A'
+alias ll='ls -aoF'
+alias la='ls -A'
+
+# When sound isn't working properly with stero jack we need to kill the audio daemon
+# OS will relaunch it and it should fix audio issues
+alias refresh_audio='sudo pkill coreaudiod'
+
+# Set to this to use case-sensitive completion
+# CASE_SENSITIVE="true"
+
+# Comment this out to disable bi-weekly auto-update checks
+# DISABLE_AUTO_UPDATE="true"
+
+# Uncomment to change how often before auto-updates occur? (in days)
+# export UPDATE_ZSH_DAYS=13
+
+# Uncomment following line if you want to disable colors in ls
+# DISABLE_LS_COLORS="true"
+
+# Uncomment following line if you want to disable autosetting terminal title.
+# DISABLE_AUTO_TITLE="true"
+
+# Uncomment following line if you want to disable command autocorrection
+# DISABLE_CORRECTION="true"
+
+# Uncomment following line if you want red dots to be displayed while waiting for completion
+COMPLETION_WAITING_DOTS="true"
+
+# Uncomment following line if you want to disable marking untracked files under
+# VCS as dirty. This makes repository status check for large repositories much,
+# much faster.
+# DISABLE_UNTRACKED_FILES_DIRTY="true"
+
+# Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
+# Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
+# Example format: plugins=(rails git textmate ruby lighthouse)
+plugins=(git, sbt)
+
+source $ZSH/oh-my-zsh.sh
+
+#export GOROOT=/usr/local/go
+export GOPATH=$HOME/go
+# Customize to your needs...
+export PATH=$HOME/bin:/opt/local/bin:/opt/local/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin:/usr/local/mysql/bin:/usr/local/java/default/bin:$GOROOT/bin:$GOPATH/bin:$HOME/.cargo/bin:$PATH
+
+
+#History Settings
+setopt inc_append_history
+setopt no_share_history
+
+#git settings
+__git_files () {
+    _wanted files expl 'local files' _files
+}
+
+export PATCHDIR=$HOME/patch
+
+# Stop annoying autocorrect
+unsetopt correct_all
+
+
